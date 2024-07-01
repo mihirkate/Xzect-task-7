2744a56281716e7974d1465298c4cdf2694c0562
mihirkate <mihirkate13@gmail.com>
2024-07-01 18:19:07 +0530

    added task-7

---
+++ src/api/hosting-plan/controllers/hosting-plan.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * hosting-plan controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::hosting-plan.hosting-plan');
---
+++ src/api/hosting-plan/routes/hosting-plan.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * hosting-plan router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::hosting-plan.hosting-plan');
